d2730befa28dd232cc4559c7aa2325b437b137b7
harshitasharma26 <harshitasharma0741@gmail.com>
2025-12-24 16:59:36 +0530

    Add files via upload

--- project.py
+++ project.py
@@ -1,14 +1,19 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
 st.title("population")
 df = pd.read_csv("census2011.csv")
 st.write(df)
-option=st.multiselect("choose your state",
-                      df["State"],placeholder= "typing")
+df["Population"] = (
+    df["Population"]
+    .astype(str)
+    .str.replace(",", "", regex=True)
+)
+df["Population"] = pd.to_numeric(df["Population"].astype(int))
+option=st.multiselect ("choose your state",
+                      df["State"], default=["Punjab","West Bengal"])
 selected_state= df[df["State"].isin(option)]
 st.write(selected_state)
-record=st.selectbox("choose your state",
-                      df["State"],placeholder= "typing")
-selected= df[df["State"]==record]
-st.write(selected)
-            
+fig=px.sunburst(selected_state,path= ["State","District"],values="Population")
+st.plotly_chart(fig)
